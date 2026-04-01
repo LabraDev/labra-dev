25e3e588eb15b310249afb1cc6b7117d5623e8fa
Gaballa <mgaballa.dev@gmail.com>
2026-04-01 02:42:09 -0700

    add sqlc

--- labra-backend/flake.nix
+++ labra-backend/flake.nix
@@ -1,33 +1,38 @@
 {
-	description = "The backend flake of Labra";
+  description = "The backend flake of Labra";
 
-	inputs = { 
-		nixpkgs.url = "github:nixos/nixpkgs";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	outputs = { self, nixpkgs, flake-utils }:
-		flake-utils.lib.eachDefaultSystem (
-		system:
-			let pkgs = nixpkgs.legacyPackages.${system};
-			in {
-				devShell = pkgs.mkShell { 
-					buildInputs = [ 
-							pkgs.go 
-							pkgs.air
-							pkgs.sqlite
-					 ]; 
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.air
+            pkgs.sqlite
+            pkgs.sqlc
+          ];
 
-					shellHook = ''
-						if [ ! -f .env ]; then
-							echo "dotenv not found, generating one from example"
-							cp .env.example .env
-						fi
-						
-						echo "Entered flake"
+          shellHook = ''
+            if [ ! -f .env ]; then
+            	echo "dotenv not found, generating one from example"
+            	cp .env.example .env
+            fi
 
-					'';
+            echo "Entered flake"
 
-				};
-			});
+          '';
+        };
+      }
+    );
 }
